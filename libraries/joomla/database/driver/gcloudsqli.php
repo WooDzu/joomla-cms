98ce16a1f3c658e3c539bf962b824a6be5530bed
WooDzu <woodzu123@gmail.com>
2013-12-14 05:39:41 -0800

    Adding Google Cloud SQL driver

---
+++ libraries/joomla/database/driver/gcloudsqli.php
@@ -0,0 +1,116 @@
+<?php
+/**
+ * @package     Joomla.Platform
+ * @subpackage  Database
+ *
+ * @copyright   Copyright (C) 2005 - 2013 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ */
+
+defined('JPATH_PLATFORM') or die;
+
+require_once dirname(__FILE__).'/mysqli.php';
+
+/**
+ * Google Cloud MySQLi database driver
+ *
+ * @package     Joomla.Platform
+ * @subpackage  Database
+ * @see         http://php.net/manual/en/book.mysqli.php
+ * @since       13.2
+ */
+class JDatabaseDriverGcloudsqli extends JDatabaseDriverMysqli
+{
+	/**
+	 * The name of the database driver.
+	 *
+	 * @var    string
+	 * @since  13.3
+	 */
+	public $name = 'gcloudsqli';
+
+	public function __construct($options)
+	{
+		// Pass initialisation through ancestry first.
+		parent::__construct($options);
+
+		// Retrieve configured options
+		$options = $this->options;
+
+		// Retrieve Google Cloud SQL socket for GAE from host string
+		list($host, $socket) = explode('|', $options['host']);
+
+		// Only use the socket if running on a deployed instance, do not use when running from the SDK
+		if (strpos($_SERVER['SERVER_SOFTWARE'], 'Google App Engine') === false)
+		{
+			$socket = null;
+		}
+		else
+		{
+			// App Engine app running on a GAE instance, set host equal to localhost to force socket usage
+			$host = 'localhost';
+		}
+
+		// Reset host and socket options for GAE
+		$options['host'] = $host;
+		$options['socket'] = $socket;
+		$options['port'] = null;
+
+		// Save the updated options
+		$this->options = $options;
+
+	}
+
+	/**
+	 * Connects to the database if needed.
+	 *
+	 * @return  void  Returns void if the database connected successfully.
+	 *
+	 * @since   13.3
+	 * @throws  \RuntimeException
+	 */
+	public function connect()
+	{
+		if ($this->connection)
+		{
+			return;
+		}
+
+		// If we are not running under Google App Engine we can use the regular MySQLi driver
+		if (!isset($_SERVER['SERVER_SOFTWARE']) || strpos($_SERVER['SERVER_SOFTWARE'], 'Google App Engine') === false) {
+			parent::connect();
+
+			return;
+		}
+
+		// Make sure the mysqli extension for PHP is installed and enabled.
+		if (!static::isSupported())
+		{
+			throw new \RuntimeException('The Google CloudSQL requires the mysqli extension which is not available');
+		}
+
+		$this->connection = @mysqli_connect(
+			$this->options['host'], $this->options['user'], $this->options['password'], null, $this->options['port'], $this->options['socket']
+		);
+
+		// Attempt to connect to the server.
+		if (!$this->connection)
+		{
+			throw new \RuntimeException('Could not connect to Google Cloud SQL from GAE.');
+		}
+
+		// Set sql_mode to non_strict mode
+		mysqli_query($this->connection, "SET @@SESSION.sql_mode = '';");
+
+		// If auto-select is enabled select the given database.
+		if ($this->options['select'] && !empty($this->options['database']))
+		{
+			$this->select($this->options['database']);
+		}
+
+		// Set charactersets (needed for MySQL 4.1.2+).
+		$this->setUTF();
+
+		return;
+	}
+}
