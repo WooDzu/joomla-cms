44276a3f784cb0f0d76c63d204447eb2fff68be9
WooDzu <woodzu123@gmail.com>
2013-12-14 06:43:59 -0800

    Reuse sample data from mysql/mysqli Added exporter/imported/iterator and query classes for the Google Cloud MySQLi driver

---
+++ libraries/joomla/database/importer/gcloudsqli.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @package     Joomla.Platform
+ * @subpackage  Database
+ *
+ * @copyright   Copyright (C) 2005 - 2013 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ */
+
+defined('JPATH_PLATFORM') or die;
+
+require_once dirname(__FILE__).'/mysqli.php';
+
+/**
+ * Google Cloud MySQLi import driver.
+ *
+ * @package     Joomla.Platform
+ * @subpackage  Database
+ * @since       13.3
+ */
+class JDatabaseImporterGcloudmysqli extends JDatabaseImporterMysqli
+{
+
+}
